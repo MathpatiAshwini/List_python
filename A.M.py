93d929ceaf42bbf20ae9f8c9fa8a506b30be9095
Ashwini Mathpati <110845470+MathpatiAshwini@users.noreply.github.com>
2022-12-21 09:59:54 +0530

    Add files via upload

---
+++ A.M.py
@@ -0,0 +1,23 @@
+# a=["ashwini","mathpati"]
+# i=0
+# n=0
+# while i<len(a):
+#     d=a[i][n]
+#     i+=1
+#     print(d.upper(),".",end="")
+
+# l=["p","q"]
+# # [p1,p2,p3,p4,p5,q1,q2,q3,q4,q5
+# i=0
+# b=[]
+# while i<len(l):
+#     n=int(l[i])
+#     j=1
+#     while j<=5:
+#         m=l[i]*l[j]
+#         b.append(m)
+#         j+=1
+#     i+=1
+# print(b)
+
+
---
+++ basic2.py
@@ -0,0 +1,44 @@
+# a=[1,2,3,4]
+# a.insert(-1,"watermelon")
+# print(a)
+
+# a=[1,2,3,4]
+# a.insert(3,3)
+# print(a)
+
+
+# a=[1,2,3,4]
+# a.insert(1,2,"gela")
+# print(a)
+
+# a[1,2,3,4]
+# a.insert(0-1,"r")
+# print(a)
+
+# a=[1,2,3,4]
+# a.insert(1+1,"r")
+# print(a)
+
+# a=[1,2,3,4]
+# a.insert(2*2,"r")
+# print(a)
+
+# a=[1,2,3,4]
+# b=a.insert(2,"k")
+# print(b)
+
+# a=["2","str","int",8,2.4,"4j","55"]
+# a.insert(3,"w")
+# print(a)
+
+# a=[1,2,3,4]
+# a.insert(3)
+# print(a)
+
+# a=[1,2,3,4]
+# a.insert()
+# print(a)
+
+# a=[1,2,3,4]
+# a.insert(3,"re")
+# print(a)
